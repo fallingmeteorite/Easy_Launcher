d3bd9449582343b3f8007e1dfafb6a153d0b80ae
Fallingmeteorite <86793086+fallingmeteorite@users.noreply.github.com>
2024-06-14 11:37:37 +0800

    Update modelCardshow.xaml.cs

--- modelCardshow.xaml.cs
+++ modelCardshow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using Awake.Views.Windows;
 using System;
 using System.IO;
+using System.Net;
 using System.Net.Http;
+using System.Security.Policy;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -16,6 +18,7 @@ namespace Awake
     {
         public static bool 允许打开模型 = true;
 
+        private bool check_url;
         string 模型_UUID = "";
         string _nickname = "";
         string _avatar = "";
@@ -25,45 +28,49 @@ namespace Awake
         {
             InitializeComponent();
 
+            try
+            {
+                HttpWebRequest myHttpWebRequest = (HttpWebRequest)WebRequest.Create(avatar);
+                myHttpWebRequest.UserAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko";
+                myHttpWebRequest.Method = "GET";
+                HttpWebResponse myHttpWebResponse = (HttpWebResponse)myHttpWebRequest.GetResponse();
+            }
+            catch
+            { }
+            try
+            {
+                HttpWebRequest myHttpWebRequest = (HttpWebRequest)WebRequest.Create(imageUrl);
+                myHttpWebRequest.UserAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko";
+                myHttpWebRequest.Method = "GET";
+                HttpWebResponse myHttpWebResponse = (HttpWebResponse)myHttpWebRequest.GetResponse();
+            }
+            catch
+            { }
 
-            //模型名称.Text = modelName;
             LoadImageFromUrlAsync(imageUrl, avatar);
+
             async Task LoadImageFromUrlAsync(string imageUrl, string avatar)
             {
-
-
                 // 将图片设置为Image控件的Source
-
                 try
                 {
-                    HttpClient client1 = new HttpClient();
-                    try
+                    HttpClient client2 = new HttpClient();
+                    var imageBytes2 = await client2.GetByteArrayAsync(imageUrl);
+                    var image2 = new BitmapImage();
+                    image2.BeginInit();
+                    image2.CacheOption = BitmapCacheOption.OnLoad;
+                    image2.CreateOptions = BitmapCreateOptions.DelayCreation;
+                    image2.StreamSource = new MemoryStream(imageBytes2);
+                    image2.EndInit();
+                    if (check_url == true)
                     {
-                        var imageBytes1 = await client1.GetByteArrayAsync(imageUrl);
-                        var image1 = new BitmapImage();
-                        image1.BeginInit();
-                        image1.CacheOption = BitmapCacheOption.OnLoad;
-                        image1.CreateOptions = BitmapCreateOptions.DelayCreation;
-                        image1.StreamSource = new MemoryStream(imageBytes1);
-                        image1.EndInit();
-                        //BitmapImage 模型封面图 = new BitmapImage(new Uri(imageUrl));
-                        模型封面.ImageSource = image1;
+                        File.WriteAllText(@".\logs\error.txt", check_url.ToString());
+                        模型封面.ImageSource = image2;
                     }
-                    catch (Exception error)
-                    {
-                        string str1 = error.Message;
-                        File.WriteAllText(@".\logs\error.txt", str1);
-                        throw;
-                    }
-
-
                 }
-                catch (System.IO.IOException ex)
+                catch (Exception error)
                 {
-                    string str1 = ex.Message;
-                    File.WriteAllText(@".\logs\error.txt", str1);
-                    throw;
-
+                    File.WriteAllText(@".\logs\error.txt", error.Message.ToString());
                 }
 
                 try
@@ -71,46 +78,32 @@ namespace Awake
                     HttpClient client1 = new HttpClient();
 
                     var imageBytes1 = await client1.GetByteArrayAsync(avatar);
-                    try
-                    {
-                        var image1 = new BitmapImage();
-                        image1.BeginInit();
-                        image1.CacheOption = BitmapCacheOption.OnLoad;
-                        image1.CreateOptions = BitmapCreateOptions.DelayCreation;
-                        image1.StreamSource = new MemoryStream(imageBytes1);
-                        image1.EndInit();
-                        作者头像.ImageSource = image1;
-                    }
-                    catch (Exception error)
-                    {
-                        string str1 = error.Message;
-                        File.WriteAllText(@".\logs\error.txt", str1);
-                        throw;
-                    }
 
-
-                    //BitmapImage _avatarimage= new BitmapImage(new Uri(avatar));
-
-
+                    var image1 = new BitmapImage();
+                    image1.BeginInit();
+                    image1.CacheOption = BitmapCacheOption.OnLoad;
+                    image1.CreateOptions = BitmapCreateOptions.DelayCreation;
+                    image1.StreamSource = new MemoryStream(imageBytes1);
+                    image1.EndInit();
+                    作者头像.ImageSource = image1;
                 }
-                catch (
-             System.IO.IOException ex)
+                catch (Exception error)
                 {
-                    string str1 = ex.Message;
-                    File.WriteAllText(@".\logs\error.txt", str1);
-                    throw;
+                    File.WriteAllText(@".\logs\error.txt", error.Message.ToString());
+                    check_url = false;
                 }
+
+                模型名称.Text = modelname;
+                模型类型.Text = modelTypeName;
+                作者名称.Text = nickname;
+                模型_UUID = uuid;
+                _nickname = nickname;
+                _avatar = avatar;
+                _modelType = modelTypeName;
+                _imageURL = imageUrl;
 
             }
 
-            模型名称.Text = modelname;
-            模型类型.Text = modelTypeName;
-            作者名称.Text = nickname;
-            模型_UUID = uuid;
-            _nickname = nickname;
-            _avatar = avatar;
-            _modelType = modelTypeName;
-            _imageURL = imageUrl;
         }
 
         private void _modelCard_Click(object sender, RoutedEventArgs e)
